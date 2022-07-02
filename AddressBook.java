a399514b04f3c4e87f0b1746635f3268ec0c6e6b
suraj <surajmore2143@gmail.com>
2022-07-02 16:05:16 +0530

    Added to edit contact in AddressBook feature

--- AddressBook.java
+++ AddressBook.java
@@ -12,6 +12,7 @@ public class AddressBook {
         while (isAdd) {
             System.out.println("Press 1 for add contact :");
             System.out.println("Press 2 for Display contact :");
+            System.out.println("Press 3 for Edit contact : ");
             System.out.println("Press 0 for Exit contact : ");
             int choice = scanner.nextInt();
             switch (choice) {
@@ -20,6 +21,9 @@ public class AddressBook {
                     break;
                 case 2:
                     displayPerson();
+                    break;
+                case 3:
+                    editPerson();
                     break;
                 default:
                     isAdd = false;
@@ -43,5 +47,23 @@ public class AddressBook {
         System.out.println("___________________________________");
         System.out.println("List is :  " + list);
 
+    }
+    public static void editPerson() {
+        System.out.println("Please enter your first name");
+        String personName = scanner.next();
+        System.out.println("___________________________________");
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i).getFirstname().equals(personName)){
+                System.out.println("Please Enter first Name");
+                String firstname = scanner.next();
+                System.out.println("Enter last Name");
+                String lastname = scanner.next();
+                System.out.println("Enter address");
+                String address = scanner.next();
+                list.get(i).getFirstname();
+                list.get(i).getLastname();
+                list.get(i).setAddress(address);
+            }
+        }
     }
 }
